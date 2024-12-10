2e7d5589ef113521cb39c716330d5bf49d32d531
FatimaHoil <LE23080267@merida.tecnm.mx>
2024-12-10 11:21:26 -0600

    doctor_appointment_app

---
+++ lib/components/social_button.dart
@@ -0,0 +1,34 @@
+import 'package:doctor_appointment_app/utils/config.dart';
+import 'package:flutter/material.dart';
+
+class SocialButton extends StatelessWidget {
+  const SocialButton({super.key, required this.social});
+
+  final String social;
+
+  @override
+  Widget build(BuildContext context) {
+    return  OutlinedButton(
+      onPressed: () {},
+      child: SizedBox(
+        width: Config.widthSize * 0.4,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset(
+              'asset/$social.png',
+              width: 40,
+              height: 40,
+            ),
+            Text(
+              social.toUpperCase(),
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/auth_page.dart
@@ -0,0 +1,76 @@
+import 'package:doctor_appointment_app/components/login_form.dart';
+import 'package:doctor_appointment_app/utils/config.dart';
+import 'package:doctor_appointment_app/utils/text.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  @override
+  Widget build(BuildContext context) {
+    Config().init(context);
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 15,
+      ),
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              AppText.enText['welcome_text']!,
+              style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            ),
+            Config.spaceSmall,
+            Text(
+              AppText.enText['singIn_Text']!,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Config.spaceSmall,
+            const LoginForm(),
+            Config.spaceSmall,
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  AppText.enText['forgot-password']!,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+            //Add social button sign in
+            const Spacer(),
+            Center(
+              child: Text(
+                AppText.enText['social-login']!,
+                style: TextStyle(
+                    fontSize: 16, 
+                    fontWeight: FontWeight.normal,
+                    color:  Colors.grey.shade500,
+                ),
+              ),
+            ),
+            Config.spaceSmall,
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //let's build social
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
